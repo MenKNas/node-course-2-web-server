5de4e2f0c933ba20cb3fe0c5120e4c1e3dbd3b03
Menelaos <men.nasies@gmail.com>
2018-04-30 18:51:34 +0300

    commit before deploying to heroku

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
@@ -52,6 +54,6 @@ app.get("/bad",(req,res)=>{
     });
 });
 
-app.listen(3000,()=>{
-    console.log("============   Server up and Running   ============")
+app.listen(port,() => {
+    console.log(`============   Server up and Running   ======  ${port}  ======`);
 });
